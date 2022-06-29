2a790e4d9209621934141a05cf5c90b8c653eaa8
mRiver <river.mst@gmail.com>
2022-06-29 12:33:06 +0300

    Добавлена возможность редактировать комментарии

--- InterviewsApp.Client/InterviewsApp.WebApp/wwwroot/js/details-pages/interview-details.js
+++ InterviewsApp.Client/InterviewsApp.WebApp/wwwroot/js/details-pages/interview-details.js
@@ -25,3 +25,56 @@
             })
     });
 }
+function editComment() {
+    let com = document.getElementById("comment");
+    let commInput = document.getElementById("comment-edit-input");
+    commInput.value = com.innerText;
+    com.style = "display:none;";
+    commInput.parentElement.style = "";
+    document.getElementById("edit-comment").parentElement.style = "display:none;";
+    document.getElementById("confirm-comment").parentElement.style = "";
+    document.getElementById("discard-comment").parentElement.style = "";
+
+}
+function discardComment() {
+    let comm = document.getElementById("comment");
+    let commInput = document.getElementById("comment-edit-input");
+    commInput.value = comm.innerText;
+    comm.style = "";
+    commInput.parentElement.style = "display:none;";
+    document.getElementById("edit-comment").parentElement.style = "";
+    document.getElementById("confirm-comment").parentElement.style = "display:none;";
+    document.getElementById("discard-comment").parentElement.style = "display:none;";
+
+}
+function confirmComment(id) {
+    document.getElementById("confirm-comment").disabled = true;
+    document.getElementById("comment-edit-input").disabled = true;
+    fetch(apihost + "/Interview/UpdateComment",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + sessionStorage.getItem(tokenKey)
+            },
+            body: JSON.stringify({
+                id: id,
+                userId: sessionStorage.getItem(currentUserId),
+                comment: document.getElementById("comment-edit-input").value
+            })
+        }).then((response) => {
+            if (response.ok === true) {
+                location.reload();
+            }
+            else {
+                document.getElementById("confirm-comment").disabled = false;
+                document.getElementById("comment-edit-input").disabled = false;
+                response.json().then((data) => {
+                    if (data.errors) {
+                        setMessage("");
+                        addRequestErrorsToMessage(data);
+                    }
+                });
+            };
+        });
+}
