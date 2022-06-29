d6645a7c7572d96cab1a93830c761ff252aeda4a
mRiver <river.mst@gmail.com>
2022-06-29 15:09:54 +0300

    Добавлены иконки и элемент для оценки компании

--- InterviewsApp.Client/InterviewsApp.WebApp/wwwroot/js/details-pages/company-details.js
+++ InterviewsApp.Client/InterviewsApp.WebApp/wwwroot/js/details-pages/company-details.js
@@ -16,3 +16,43 @@
             })
     });
 }
+function handleHover(event) {
+    let num = event.target.id.substring(5);
+    for (let i = 0; i <= 50; i += 5) {
+        let elem = document.getElementById("star-" + i);
+        elem.classList.value = "";
+        if (i % 10 > 0) {
+            if (i < num) {
+                elem.classList.add("fa-solid");
+                elem.classList.add("fa-star");
+            }
+            else
+                if (i == num) {
+                    elem.classList.add("fa-solid");
+                    elem.classList.add("fa-star-half-stroke");
+                }
+                else {
+                    elem.classList.add("fa-regular");
+                    elem.classList.add("fa-star");
+                }
+        }
+        else if (i == num){
+            let prevelem = document.getElementById("star-" + (i - 5));
+            if (prevelem) {
+                elem.classList.value = "";
+                prevelem.classList.add("fa-solid");
+                prevelem.classList.add("fa-star");
+            }
+        }
+    }
+}
+function clearStar() {
+    for (let i = 0; i <= 50; i += 5) {
+        if (i % 10 > 0) {
+            let elem = document.getElementById("star-" + i);
+            elem.classList.value = "";
+            elem.classList.add("fa-regular");
+            elem.classList.add("fa-star");
+        }
+    }
+}
